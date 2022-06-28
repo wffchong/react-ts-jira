89ce7475b148007fdb36a06d22f22c640fb4e2c8
wffccc <fufanwang@pawnfi.com>
2022-06-28 16:19:53 +0800

    feat: 添加dev-tool

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { loadDevTools } from 'jira-dev-tool'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+loadDevTools(() =>
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    )
 )
 
 // If you want to start measuring performance in your app, pass a function
